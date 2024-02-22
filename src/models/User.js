80e941b3929906043663df5d429873c4180c7554
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-22 15:15:02 +0900

    feat: 회원 가입 API 추가

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const { connectDB } = require("../utils/db");
+const bcrypt = require("bcrypt");
+const Playlist = require("../models/Playlist");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -21,6 +24,39 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.isDuplicatedEmail = async function (email) {
+  const user = await this.findOne({ email: email });
+  return user !== null;
+};
+
+userSchema.statics.signUp = async function (email = null, nickname, password) {
+  try {
+    const salt = await bcrypt.genSalt();
+    console.log(salt);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const isMemeber = email ? true : false;
+
+    // 빈 플레이 리스트 부여
+    const playlist = (await Playlist.create({ musics: [] }))._id;
+    const acceptPlaylist = (await Playlist.create({ musics: [] }))._id;
+    const rejectPlaylist = (await Playlist.create({ musics: [] }))._id;
+    const user = await this.create({
+      email,
+      nickname,
+      password: hashedPassword,
+      isMemeber,
+      playlist,
+      acceptPlaylist,
+      rejectPlaylist,
+    });
+
+    return { _id: user._id, nickname: user.nickname };
+  } catch (err) {
+    throw err;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- src/routes/user.js
+++ src/routes/user.js
@@ -12,4 +12,29 @@ router.get("/", function (req, res, next) {
     });
 });
 
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { email, nickname, password } = req.body;
+    console.log(req.body);
+
+    // 이메일 중복 체크
+    const isDuplicatedEmail = await User.isDuplicatedEmail(email);
+
+    if (isDuplicatedEmail)
+      return res.status(409).json({
+        error: {
+          title: "Bad request",
+          msg: "This email is already in use. Please use a different email.",
+        },
+      });
+    else {
+      const user = await User.signUp(email, nickname, password);
+      res.status(201).json(user);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
 module.exports = router;
