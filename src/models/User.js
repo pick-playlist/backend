530d71752b7e04ce496a80b69123a08e875e6f18
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-22 16:25:54 +0900

    feat: 회원, 비회원 로그인 API 추가

--- src/models/User.js
+++ src/models/User.js
@@ -4,25 +4,34 @@ const bcrypt = require("bcrypt");
 const Playlist = require("../models/Playlist");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: false },
   nickname: { type: String, required: true },
-  password: { type: String, required: true },
-  isMemeber: { type: Boolean, required: true },
+  password: { type: String, required: false },
+  isMember: { type: Boolean, required: true },
   playlist: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Playlist",
-    required: true,
+    required: false,
   },
   acceptPlaylist: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Playlist",
-    required: true,
+    required: false,
   },
   rejectPlaylist: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Playlist",
-    required: true,
+    required: false,
   },
+});
+
+const visibleUser = userSchema.virtual("visibleUser");
+visibleUser.get(function (value, virtual, doc) {
+  return {
+    _id: doc._id,
+    email: doc.email,
+    nickname: doc.nickname,
+  };
 });
 
 userSchema.statics.isDuplicatedEmail = async function (email) {
@@ -36,7 +45,7 @@ userSchema.statics.signUp = async function (email = null, nickname, password) {
     console.log(salt);
 
     const hashedPassword = await bcrypt.hash(password, salt);
-    const isMemeber = email ? true : false;
+    const isMemeber = true;
 
     // 빈 플레이 리스트 부여
     const playlist = (await Playlist.create({ musics: [] }))._id;
@@ -56,6 +65,25 @@ userSchema.statics.signUp = async function (email = null, nickname, password) {
   } catch (err) {
     throw err;
   }
+};
+
+userSchema.statics.memberLogin = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+
+    if (auth) {
+      return user.visibleUser;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+userSchema.statics.guestLogin = async function (nickname) {
+  const isMember = false;
+  const user = await this.create({ nickname, isMember });
+  return { _id: user._id, nickname: user.nickname };
 };
 
 const User = mongoose.model("User", userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/User");
+const { createToken, verifyToken } = require("../utils/auth");
 
 router.get("/", function (req, res, next) {
   User.find()
@@ -37,4 +38,39 @@ router.post("/signup", async (req, res, next) => {
     next(err);
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    user = await User.memberLogin(email, password);
+    const tokenMaxAge = 60 * 60 * 24 * 3;
+    const token = createToken(user, tokenMaxAge);
+
+    user.token = token;
+
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: tokenMaxAge * 1000,
+    });
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+    next(err);
+  }
+});
+
+router.post("/guest-login", async (req, res, next) => {
+  try {
+    const { nickname } = req.body;
+    const user = await User.guestLogin(nickname);
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+    next(err);
+  }
+});
+
 module.exports = router;
