738b2c41852c7c72c82685a7f96c4e13d2b9398c
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-02-23 08:26:44 +0900

    feat: create music

--- src/routes/music.js
+++ src/routes/music.js
@@ -6,6 +6,22 @@ const Playlist = require("../models/Playlist");
 const Room = require("../models/Room");
 const User = require("../models/User");
 
-router.get("/", function (req, res, next) {});
+router.post("/", async function (req, res, next) {
+  try {
+    const { title, artist, comment, userId, link } = req.body;
+
+    const newMusic = await Music.create({
+      title: title,
+      artist: artist,
+      comment: comment,
+      proposer: userId,
+      link: link,
+      agree: 0,
+      reject: 0,
+    });
+
+    res.send(newMusic);
+  } catch (err) {}
+});
 
 module.exports = router;
