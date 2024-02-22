f3622fd852ae2ce91600feaa21d6373eeb7901be
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-22 14:35:45 +0900

    feat: 모든 User 조회 API 추가

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../models/User");
 
-/* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  User.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      return next(err);
+    });
 });
 
 module.exports = router;
